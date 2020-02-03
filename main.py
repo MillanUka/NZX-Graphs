6430d5e3b36acaf2f0c544e60669be86877d2140
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-02-04 09:17:40 +1300

    Now scraps from NZDX and NZZX
    Also uses regex rather than chaining replace

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-
+import re
 
 def get_market(market):
         markets_url = "https://www.nzx.com/markets/" + market
@@ -19,18 +19,34 @@ def get_market(market):
                 html_listings = rows.find_all("td")
                 code = html_listings[0].get_text().strip()
                 company = html_listings[1].get_text().strip()
-                price = float(html_listings[2].get_text().strip().replace(",", "").replace("$", ""))
+                try:
+                     price = float(re.sub('[^0-9\d.]','', html_listings[2].get_text().strip()))
+                except:
+                    price = 0
                 change = html_listings[3].get_text().strip()
-                volume =float(html_listings[4].get_text().strip().replace(",", "").replace("$", ""))
-                value = float(html_listings[5].get_text().strip().replace(",", "").replace("$", ""))
-                capitalisation = float(html_listings[6].get_text().strip().replace(",", "").replace("$", ""))
+                try:
+                    volume = float(re.sub('[^0-9\d.]','', html_listings[4].get_text().strip()))
+                except:
+                    volume = 0
+                
+                try:
+                    value = float(re.sub('[^0-9\d.]','', html_listings[5].get_text().strip()))
+                except:
+                    value = 0
+
+                try:
+                    capitalisation = float(re.sub('[^0-9\d.]','', html_listings[6].get_text().strip()))
+                except:
+                    capitalisation = 0
+
                 market_listings.append([code, company, price, change, volume, value, capitalisation])
 
 
         with open(market+ "_data.csv", "a", encoding="UTF-8") as toWrite :
             writer = csv.writer(toWrite)
-            print(market_listings)
             writer.writerows(market_listings)
 
 
 get_market("NZSX")
+get_market("NZDX")
+get_market("NZZX")
